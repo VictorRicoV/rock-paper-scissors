b06bca5abe627f14435954d33c7b3988a1797a44
Victor <lessricc@gmail.com>
2024-07-01 18:46:21 +0200

    vamos por el simple

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+let userPoints = 0;
+let pcPoints = 0;
+let userPlay = null;
+let pcPlat = null;
+
+const gameContainerElement = document.getElementById('game-container');
+
+const handleClick = event => {
+  console.log(event.target);
+};
+
+gameContainerElement.addEventListener('click', handleClick);
